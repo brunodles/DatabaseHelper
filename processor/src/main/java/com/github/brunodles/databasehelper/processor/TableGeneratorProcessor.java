56cadf3ba28d6a4306b10dd2650df18546e8bdd0
Bruno de Lima <dlimaun@gmail.com>
2016-11-07 08:27:12 -0300

    Update Processor to manage the SqlHelpers annotation

--- processor/src/main/java/com/github/brunodles/databasehelper/processor/TableGeneratorProcessor.java
+++ processor/src/main/java/com/github/brunodles/databasehelper/processor/TableGeneratorProcessor.java
@@ -6,6 +6,7 @@ import com.github.brunodles.annotationprocessorhelper.SupportedAnnotations;
 import com.github.brunodles.classreader.ClassReader;
 import com.github.brunodles.classreader.Field;
 import com.github.brunodles.databasehelper.annotation.CreateTable;
+import com.github.brunodles.databasehelper.annotation.SqlHelpers;
 import com.github.brunodles.databasehelper.writer.MethodBuilder;
 import com.github.brunodles.databasehelper.writer.OutputClass;
 import com.github.brunodles.sqlhelper.CreateBuilder;
@@ -41,7 +42,7 @@ import static com.github.brunodles.primitiveutils.FormattableString.formattable;
 import static com.github.brunodles.primitiveutils.StringUtils.quote;
 import static java.lang.String.format;
 
-@SupportedAnnotations({CreateTable.class})
+@SupportedAnnotations({CreateTable.class, SqlHelpers.class})
 @SupportedSourceVersion(SourceVersion.RELEASE_7)
 public class TableGeneratorProcessor extends AbstractProcessorBase {
 
@@ -56,67 +57,84 @@ public class TableGeneratorProcessor extends AbstractProcessorBase {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (TypeElement te : annotations) {
             for (Element e : roundEnv.getElementsAnnotatedWith(te)) {
-                CreateTable a = e.getAnnotation(CreateTable.class);
-                String fieldReadMethod = a.fieldGetter().method;
-                String fieldWriteMethod = a.fieldSetter().method;
-                TypeElement value = asTypeElement(getMyValue1(a));
-
-                ClassReader reader = ClassReader.Factory.read(this.processingEnv, value);
-
-                String className = value.getSimpleName() + "SqlHelper";
-                String packageName = e.toString().replace("." + e.getSimpleName(), "");
-                String tableName = Utils.classNameToTableName(value.getSimpleName().toString());
-
-                OutputClass outputClass = new OutputClass(packageName, className);
-                outputClass.addImport("android.content.ContentValues")
-                        .addImport("android.support.annotation.NonNull")
-                        .addImport("android.database.Cursor")
-                        .addImport(value.getQualifiedName().toString());
-
-                outputClass.addPublicConstant("TABLE_NAME", String.class).value(quote(tableName));
-                CreateBuilder createBuilder = SqlHelper.create(tableName);
-                MethodBuilder contentValues = outputClass
-                        .addMethod("public", "createValues", "ContentValues", "static", "final")
-                        .addParam("object", reader.getClassName())
-                        .addLine("ContentValues values = new ContentValues();");
-                MethodBuilder readCursor = outputClass
-                        .addMethod("public", "readCursor", reader.getClassName(), "static", "final")
-                        .addParam("cursor", "Cursor")
-                        .addLine(String.format("%1$s object = new %1$s();", reader.getClassName()));
-
-                for (Field field : reader.getFields()) {
-                    outputClass.addImport(field.typeCanonical);
-
-                    String fieldKey = Utils.dbFieldKey(field);
-                    outputClass.addPublicConstant(fieldKey, String.class)
-                            .value(quote(field.name));
-                    createBuilder.add(field.name, Utils.dbFieldType(field));
-                    contentValues.addLine(format("values.put(%s, object." + fieldReadMethod + ");",
-                            fieldKey, field.name));
-
-                    readCursor.addLine(format("object." + fieldWriteMethod + ";", formattable(field.name),
-                            readFieldFromDb(field)));
-                }
-                readCursor.addLine("return object;");
-                contentValues.addLine("return values;");
-
-                outputClass.addPublicConstant("CREATE_TABLE", String.class)
-                        .value(quote(createBuilder.build()));
-
-                MustacheFactory mf = new DefaultMustacheFactory();
-                Mustache mustache = mf.compile("class.mustache");
-                StringWriter writer = new StringWriter();
-                try {
-                    log(TAG, String.format("build helper class -> %s.%s", packageName, className));
-                    mustache.execute(writer, outputClass).flush();
-                    log(TAG, writer.toString());
-                    writeClass(className, packageName, writer.toString());
-                } catch (IOException e1) {
-                    fatalError(TAG, e1.getMessage());
-                }
+                checkCreateTable(e);
+                checkSqlHelpers(e);
             }
         }
         return true;
+    }
+
+    private void checkSqlHelpers(Element element) {
+        SqlHelpers annotation = element.getAnnotation(SqlHelpers.class);
+        CreateTable[] value = annotation.value();
+        for (CreateTable createTable : value) {
+            buildHelperClass(element, createTable);
+        }
+    }
+
+    private void checkCreateTable(Element e) {
+        CreateTable a = e.getAnnotation(CreateTable.class);
+        if (a != null) buildHelperClass(e, a);
+    }
+
+    private void buildHelperClass(Element element, CreateTable annotation) {
+        String fieldReadMethod = annotation.fieldGetter().method;
+        String fieldWriteMethod = annotation.fieldSetter().method;
+        TypeElement value = asTypeElement(getMyValue1(annotation));
+
+        ClassReader reader = ClassReader.Factory.read(this.processingEnv, value);
+
+        String className = value.getSimpleName() + "SqlHelper";
+        String packageName = element.toString().replace("." + element.getSimpleName(), "");
+        String tableName = Utils.classNameToTableName(value.getSimpleName().toString());
+
+        OutputClass outputClass = new OutputClass(packageName, className);
+        outputClass.addImport("android.content.ContentValues")
+                .addImport("android.support.annotation.NonNull")
+                .addImport("android.database.Cursor")
+                .addImport(value.getQualifiedName().toString());
+
+        outputClass.addPublicConstant("TABLE_NAME", String.class).value(quote(tableName));
+        CreateBuilder createBuilder = SqlHelper.create(tableName);
+        MethodBuilder contentValues = outputClass
+                .addMethod("public", "createValues", "ContentValues", "static", "final")
+                .addParam("object", reader.getClassName())
+                .addLine("ContentValues values = new ContentValues();");
+        MethodBuilder readCursor = outputClass
+                .addMethod("public", "readCursor", reader.getClassName(), "static", "final")
+                .addParam("cursor", "Cursor")
+                .addLine(String.format("%1$s object = new %1$s();", reader.getClassName()));
+
+        for (Field field : reader.getFields()) {
+            outputClass.addImport(field.typeCanonical);
+
+            String fieldKey = Utils.dbFieldKey(field);
+            outputClass.addPublicConstant(fieldKey, String.class)
+                    .value(quote(field.name));
+            createBuilder.add(field.name, Utils.dbFieldType(field));
+            contentValues.addLine(format("values.put(%s, object." + fieldReadMethod + ");",
+                    fieldKey, field.name));
+
+            readCursor.addLine(format("object." + fieldWriteMethod + ";", formattable(field.name),
+                    readFieldFromDb(field)));
+        }
+        readCursor.addLine("return object;");
+        contentValues.addLine("return values;");
+
+        outputClass.addPublicConstant("CREATE_TABLE", String.class)
+                .value(quote(createBuilder.build()));
+
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache mustache = mf.compile("class.mustache");
+        StringWriter writer = new StringWriter();
+        try {
+            log(TAG, String.format("build helper class -> %s.%s", packageName, className));
+            mustache.execute(writer, outputClass).flush();
+            log(TAG, writer.toString());
+            writeClass(className, packageName, writer.toString());
+        } catch (IOException e1) {
+            fatalError(TAG, e1.getMessage());
+        }
     }
 
     private static TypeMirror getMyValue1(CreateTable annotation) {
