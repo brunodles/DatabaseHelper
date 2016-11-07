e9c8e50ad655011c27e234b068a69456b4e594a2
Bruno de Lima <dlimaun@gmail.com>
2016-11-07 08:36:22 -0300

    Update SqlHelpers to target only Type

--- annotation/src/main/java/com/github/brunodles/databasehelper/annotation/SqlHelpers.java
+++ annotation/src/main/java/com/github/brunodles/databasehelper/annotation/SqlHelpers.java
@@ -3,15 +3,13 @@ package com.github.brunodles.databasehelper.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.CLASS;
 
 /**
  * Created by bruno on 07/11/16.
  */
-@Target({TYPE, FIELD, METHOD})
+@Target({TYPE})
 @Retention(CLASS)
 public @interface SqlHelpers {
     SqlHelper[] value();
